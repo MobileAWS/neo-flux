5667ca11f82245bb4eb54ea7521d5e293602a221
Santiago Gomez <sangomezvas@unal.edu.co>
2018-11-14 18:40:15 -0500

    blockchain controller

---
+++ NeoFlux/Controllers/BlockChainController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Neo.Lux.Cryptography;
+using Neo.Lux.Utils;
+using NeoFlux.Support;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Serilog;
+
+namespace NeoFlux.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlockChainController : BaseController
+    {
+        [HttpPost]
+        [Route("get_block")]
+        [Authorize]
+        public JsonResult GetBlock([FromBody]JObject jsonData)
+        {
+            try
+            {                
+                var blockIndex = jsonData.GetValue("blockIndex").Value<uint>();
+                var blockResult = LuxApiFactory.GetLuxApi().GetBlock(blockIndex);
+                if (blockResult != null)
+                {
+                    dynamic result = new JObject();
+                    result.version = blockResult.Version;
+                    result.height = blockResult.Height;
+                    result.merkleRoot = blockResult.MerkleRoot;
+                    result.timestamp = blockResult.Timestamp;
+                    result.timestamp = blockResult.Timestamp;
+                    result.previousHash = blockResult.PreviousHash.ToString();
+                    dynamic transactionIds = new JArray();
+                    foreach (var transaction in blockResult.transactions)
+                    {
+                        string key = transaction.Hash.ToString();
+                        transactionIds.Add(key);
+                    }
+                    result.transactions = transactionIds;
+                    return Json(result);
+                }
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message + "\n" + e.StackTrace);
+                return JsonError($"Unable to get block information, {e.Message}");
+            }
+        }
+        
+        [HttpPost]
+        [Route("get_best_block")]
+        [Authorize]
+        public JsonResult GetBestBlock([FromBody]JObject jsonData)
+        {
+            try
+            {                
+                var bestBlockIndex = LuxApiFactory.GetLuxApi().GetBlockHeight();
+                var blockResult = LuxApiFactory.GetLuxApi().GetBlock(bestBlockIndex);
+                if (blockResult != null)
+                {
+                    dynamic result = new JObject();
+                    result.version = blockResult.Version;
+                    result.height = blockResult.Height;
+                    result.merkleRoot = blockResult.MerkleRoot;
+                    result.timestamp = blockResult.Timestamp;
+                    result.timestamp = blockResult.Timestamp;
+                    result.previousHash = blockResult.PreviousHash.ToString();
+                    dynamic transactionIds = new JArray();
+                    foreach (var transaction in blockResult.transactions)
+                    {
+                        string key = transaction.Hash.ToString();
+                        transactionIds.Add(key);
+                    }
+                    result.transactions = transactionIds;
+                    return Json(result);
+                }
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message + "\n" + e.StackTrace);
+                return JsonError($"Unable to get block information, {e.Message}");
+            }
+        }
+        
+        [HttpPost]
+        [Route("get_block_count")]
+        [Authorize]
+        public JsonResult GetBlockCount([FromBody]JObject jsonData)
+        {
+            try
+            {                
+                var bestBlockIndex = LuxApiFactory.GetLuxApi().GetBlockHeight();
+                return JsonResultObject(bestBlockIndex);
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message + "\n" + e.StackTrace);
+                return JsonError($"Unable to get block information, {e.Message}");
+            }
+        }
+    }
+}
--- NeoFlux/Controllers/TransactionController.cs
+++ NeoFlux/Controllers/TransactionController.cs
@@ -50,5 +50,54 @@ namespace NeoFlux.Controllers
                 return null;
             });
         }
+        
+        [HttpPost]
+        [Route("get_info")]
+        [Authorize]
+        public JsonResult GetTransaction([FromBody]JObject jsonData)
+        {
+            try
+            {                
+                var txId = jsonData.GetValue("txId").Value<string>();
+                var transactionResult = LuxApiFactory.GetLuxApi().GetTransaction(txId);
+                if (transactionResult != null)
+                {
+                    dynamic result = new JObject();
+                    result.hash = transactionResult.Hash.ToString();
+                    result.type = transactionResult.type;
+                    result.version = transactionResult.version;
+                    result.block = transactionResult.block;
+                    dynamic inputs = new JArray();
+                    foreach (var input in transactionResult.inputs)
+                    {
+                        dynamic inputJson = new JObject();
+                        inputJson.prevHash = input.prevHash.ToString();
+                        inputJson.prevIndex = input.prevIndex;
+                        inputs.Add(inputJson);
+
+                    }
+                    result.inputs = inputs;
+                    dynamic outputs = new JArray();
+                    foreach (var output in transactionResult.outputs)
+                    {
+                        dynamic outputJson = new JObject();
+                        outputJson.asset = output.assetID;
+                        outputJson.address = output.scriptHash.ToAddress();
+                        outputJson.value = output.value;
+                        outputs.Add(outputJson);
+
+                    }
+                    result.outputs = outputs;
+                    return Json(result);
+                }
+
+                return null;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message + "\n" + e.StackTrace);
+                return JsonError($"Unable to get transaction information, {e.Message}");
+            }
+        }
     }
 }
